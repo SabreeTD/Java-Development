aa0c103751eeebb69fd0db0e85e5887b6f077e56
SabreeTD <sabreetd@gmail.com>
2023-10-04 10:47:46 -0400

    completed ShoppingList app

---
+++ workbook-1/Exercise1/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+
+
+    public class ShoppingList {
+        public static void main(String[] args) {
+            System.out.println("Milk");
+            System.out.println("Eggs");
+            System.out.println("Bread");
+            System.out.println("Jelly");
+            System.out.println("Peanut Butter");
+            System.out.println("Chicken");
+            System.out.println("Beef");
+            System.out.println("Fish");
+            System.out.println("Noodles");
+            System.out.println("Sauce");
+
+
+        }
+    }
+
